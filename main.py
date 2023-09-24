97dcdc930878477168b38c4c2cba7ce3d3e729ef
BryanjCruz <bryan.cruz@uip.edu.pa>
2023-09-24 16:30:30 -0500

    actualizacion

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os
 import time
 import sys
 
-# conexion con la BDD
+# Conexion con la BDD
 conn = sqlite3.connect('slang_panameno.db')
 cursor = conn.cursor()
 
-#
+# Creacion de la tabla si no existe
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS palabras (
         palabra TEXT PRIMARY KEY,
@@ -17,85 +17,92 @@ cursor.execute('''
 
 conn.commit()
 
-# funciones para las opciones del menú , animaciones y limpiesa de pantalla
-def limpiar_pantalla():
+# Funciones para las opciones del menu, animaciones y limpieza de pantalla
+def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def animacion_carga():
-    # Caracteres que forman la animación
-    animacion_chars = ['|', '/', '-', '\\']
+def loading_animation():
+    # Caracteres que forman la animacion
+    animation_chars = ['|', '/', '-', '\\']
     tempo = 0
     while True:
-        tempo=tempo+1
-        for char in animacion_chars:
+        tempo = tempo + 1
+        for char in animation_chars:
             sys.stdout.write('\rCargando ' + char)
             sys.stdout.flush()
             time.sleep(0.1)
         if tempo == 5:
             break
-def agregar_palabra():
-    palabra = input("Ingrese la nueva palabra: ")
-    significado = input("Ingrese el significado de la palabra: ")
+
+# Funciones para las opciones del menu
+def add_word():
+    word = input("Ingrese la nueva palabra: ")
+    meaning = input("Ingrese el significado de la palabra: ")
 
     try:
-        cursor.execute('INSERT INTO palabras VALUES (?, ?)', (palabra, significado))
+        cursor.execute('INSERT INTO palabras VALUES (?, ?)', (word, meaning))
         conn.commit()
         print("Palabra agregada correctamente.")
     except sqlite3.IntegrityError:
         print("La palabra ya existe en la base de datos.")
-def editar_palabra():
-    palabra = input("Ingrese la palabra que desea editar: ")
-    cursor.execute('SELECT * FROM palabras WHERE palabra=?', (palabra,))
+
+def edit_word():
+    word = input("Ingrese la palabra que desea editar: ")
+    cursor.execute('SELECT * FROM palabras WHERE palabra=?', (word,))
     row = cursor.fetchone()
     if row:
-        nuevo_significado = input("Ingrese el nuevo significado de la palabra: ")
-        cursor.execute('UPDATE palabras SET significado=? WHERE palabra=?', (nuevo_significado, palabra))
+        new_meaning = input("Ingrese el nuevo concepto: ")
+        cursor.execute('UPDATE palabras SET significado=? WHERE palabra=?', (new_meaning, word))
         conn.commit()
-        print("Palabra editada correctamente.")
+        print("Editada correctamente.")
     else:
         print("La palabra no existe en la base de datos.")
-def eliminar_palabra():
-    palabra = input("Ingrese la palabra que desea eliminar: ")
-    cursor.execute('SELECT * FROM palabras WHERE palabra=?', (palabra,))
+        loading_animation()
+
+def delete_word():
+    word = input("Ingrese la palabra que desea eliminar: ")
+    cursor.execute('SELECT * FROM palabras WHERE palabra=?', (word,))
     row = cursor.fetchone()
     if row:
-        cursor.execute('DELETE FROM palabras WHERE palabra=?', (palabra,))
+        cursor.execute('DELETE FROM palabras WHERE palabra=?', (word,))
         conn.commit()
         print("Palabra eliminada correctamente.")
+        loading_animation()
     else:
         print("La palabra no existe en la base de datos.")
-def ver_listado_palabras():
+        loading_animation()
+
+def view_word_list():
     cursor.execute('SELECT * FROM palabras')
-    palabras = cursor.fetchall()
-    if palabras:
-        limpiar_pantalla()
+    words = cursor.fetchall()
+    if words:
+        clear_screen()
         print("\t\t\tListado de palabras y significados:")
-        for palabra, significado in palabras:
-            print(f"{palabra}: {significado}")
+        for word, meaning in words:
+            print(f"{word}: {meaning}")
         input("\n\n\t\t Precione ENTER para continuar.....")
-        animacion_carga()
+        loading_animation()
     else:
         print("No hay palabras en la base de datos.")
         input("\n\n\t\t Precione ENTER tecla para continuar.....")
-        animacion_carga()
-def buscar_significado():
-    palabra = input("Ingrese la palabra que desea buscar: ")
+        loading_animation()
 
-    cursor.execute('SELECT * FROM palabras WHERE palabra=?', (palabra,))
+def search_meaning():
+    word = input("Ingrese la palabra que desea buscar: ")
+
+    cursor.execute('SELECT * FROM palabras WHERE palabra=?', (word,))
     row = cursor.fetchone()
 
     if row:
-        print(f"Significado de '{palabra}': {row[1]}")
+        print(f"Significado de '{word}': {row[1]}")
         input("\n\n\t\t Precione ENTER tecla para continuar.....")
     else:
         print("La palabra no existe en la base de datos.")
-        animacion_carga()
+        loading_animation()
 
-
-# Menú de opciones
-
-def mostrar_menu():
-    limpiar_pantalla()
+# Menu de opciones
+def show_menu():
+    clear_screen()
     print("\t\t\t\t DICCIONARIO")
     print("\nOpciones:")
     print("\t\ta) Agregar nueva palabra")
@@ -105,27 +112,25 @@ def mostrar_menu():
     print("\t\te) Buscar significado de palabra")
     print("\t\tf) Salir")
 
-
-
-# Función principal
+# Funcion principal
 if __name__ == "__main__":
     while True:
-        mostrar_menu()
-        opcion = input("Seleccione una opción: ").lower()
+        show_menu()
+        option = input("Seleccione una opción: ").lower()
 
-        if opcion == 'a':
-            agregar_palabra()
-        elif opcion == 'b':
-            editar_palabra()
-        elif opcion == 'c':
-            eliminar_palabra()
-        elif opcion == 'd':
-            ver_listado_palabras()
-        elif opcion == 'e':
-            buscar_significado()
-        elif opcion == 'f':
+        if option == 'a':
+            add_word()
+        elif option == 'b':
+            edit_word()
+        elif option == 'c':
+            delete_word()
+        elif option == 'd':
+            view_word_list()
+        elif option == 'e':
+            search_meaning()
+        elif option == 'f':
             break
         else:
             print("Opción no válida. Inténtelo de nuevo.")
-            animacion_carga()
+            loading_animation()
 
